0aa0de9c1c9945478354524f5e98b4eb13305204
Anton Baskanov <baskanov@gmail.com>
2025-10-27 13:08:12 +0700

    mmdevapi: Add a stub SW synth device.

---
+++ dlls/mmdevapi/swmidi.c
@@ -0,0 +1,45 @@
+/*
+ * MIDI driver for software synthesizer
+ *
+ * Copyright 2025 Anton Baskanov
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#include <stdarg.h>
+
+#include "windef.h"
+#include "mmddk.h"
+
+#include "wine/debug.h"
+
+WINE_DEFAULT_DEBUG_CHANNEL(midi);
+
+DWORD swmidi_mod_message(UINT dev_id, UINT msg, DWORD_PTR user, DWORD_PTR param1, DWORD_PTR param2)
+{
+    TRACE("dev_id %u, msg %x, user %#Ix, param1 %#Ix, param2 %#Ix.\n", dev_id, msg, user, param1,
+            param2);
+
+    switch (msg)
+    {
+    case DRVM_INIT:
+    case DRVM_EXIT:
+        return MMSYSERR_NOERROR;
+    case MODM_GETNUMDEVS:
+        return 1;
+    }
+
+    return MMSYSERR_NOTSUPPORTED;
+}
