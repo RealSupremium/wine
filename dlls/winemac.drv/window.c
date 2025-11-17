a0f841b30adbc06455eb3f01767aec555fc37dd9
Brendan Shanks <bshanks@codeweavers.com>
2025-11-17 14:50:31 -0800

    winemac: Use C99 bool for internal functions/variables in macdrv.h.

--- dlls/winemac.drv/window.c
+++ dlls/winemac.drv/window.c
@@ -220,7 +220,7 @@ void release_win_data(struct macdrv_win_data *data)
  *
  * Return the Mac window associated with the full area of a window
  */
-macdrv_window macdrv_get_cocoa_window(HWND hwnd, BOOL require_on_screen)
+macdrv_window macdrv_get_cocoa_window(HWND hwnd, bool require_on_screen)
 {
     struct macdrv_win_data *data = get_win_data(hwnd);
     macdrv_window ret = NULL;
@@ -2085,14 +2085,14 @@ void macdrv_app_quit_requested(const macdrv_event *event)
  *
  * Handler for QUERY_RESIZE_SIZE query.
  */
-BOOL query_resize_size(HWND hwnd, macdrv_query *query)
+bool query_resize_size(HWND hwnd, macdrv_query *query)
 {
     struct macdrv_win_data *data = get_win_data(hwnd);
     RECT rect;
     int corner;
-    BOOL ret = FALSE;
+    bool ret = false;
 
-    if (!data) return FALSE;
+    if (!data) return false;
 
     rect = rect_from_cgrect(query->resize_size.rect);
     rect = window_rect_from_visible(&data->rects, rect);
@@ -2113,7 +2113,7 @@ BOOL query_resize_size(HWND hwnd, macdrv_query *query)
     {
         rect = visible_rect_from_window(&data->rects, rect);
         query->resize_size.rect = cgrect_from_rect(rect);
-        ret = TRUE;
+        ret = true;
     }
 
     release_win_data(data);
@@ -2126,7 +2126,7 @@ BOOL query_resize_size(HWND hwnd, macdrv_query *query)
  *
  * Handler for QUERY_RESIZE_START query.
  */
-BOOL query_resize_start(HWND hwnd)
+bool query_resize_start(HWND hwnd)
 {
     TRACE("hwnd %p\n", hwnd);
 
@@ -2135,7 +2135,7 @@ BOOL query_resize_start(HWND hwnd)
     sync_window_min_max_info(hwnd);
     send_message(hwnd, WM_ENTERSIZEMOVE, 0, 0);
 
-    return TRUE;
+    return true;
 }
 
 
@@ -2144,11 +2144,11 @@ BOOL query_resize_start(HWND hwnd)
  *
  * Handler for QUERY_MIN_MAX_INFO query.
  */
-BOOL query_min_max_info(HWND hwnd)
+bool query_min_max_info(HWND hwnd)
 {
     TRACE("hwnd %p\n", hwnd);
     sync_window_min_max_info(hwnd);
-    return TRUE;
+    return true;
 }
 
 
