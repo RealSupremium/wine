e70c15a64866821116558b8d607d382d237db2d6
Marc-Aurel Zent <mzent@codeweavers.com>
2026-01-12 13:49:32 +0100

    ntdll: Subtract InterruptTimeBias in RtlQueryUnbiasedInterruptTime().

--- dlls/ntdll/time.c
+++ dlls/ntdll/time.c
@@ -463,6 +463,7 @@ NTSTATUS WINAPI RtlSetTimeZoneInformation( const RTL_TIME_ZONE_INFORMATION *tzin
 BOOL WINAPI RtlQueryUnbiasedInterruptTime(ULONGLONG *time)
 {
     ULONG high, low;
+    ULONGLONG bias;
 
     if (!time)
     {
@@ -474,9 +475,9 @@ BOOL WINAPI RtlQueryUnbiasedInterruptTime(ULONGLONG *time)
     {
         high = user_shared_data->InterruptTime.High1Time;
         low = user_shared_data->InterruptTime.LowPart;
+        bias = user_shared_data->InterruptTimeBias;
     }
     while (high != user_shared_data->InterruptTime.High2Time);
-    /* FIXME: should probably subtract InterruptTimeBias */
-    *time = (ULONGLONG)high << 32 | low;
+    *time = ((ULONGLONG)high << 32 | low) - bias;
     return TRUE;
 }
