69964b90aa66d8fbc07dd859a455b3bfbd33f8af
Konstantin Demin <rockdrilla@gmail.com>
2026-01-29 20:19:13 +0300

    winebuild: support LTO builds.
    Signed-off-by: Konstantin Demin <rockdrilla@gmail.com>

--- tools/winebuild/build.h
+++ tools/winebuild/build.h
@@ -344,6 +344,7 @@ extern void put_pword( unsigned int val );
 
 extern int current_line;
 extern int UsePIC;
+extern int UseLTO; /* tristate */
 extern int nb_errors;
 extern int display_warnings;
 extern int kill_at;
