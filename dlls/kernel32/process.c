7ef8696e9da97e2ce0f25cea20ddb53e942fede8
Konstantin Demin <rockdrilla@gmail.com>
2026-01-29 20:19:35 +0300

    kernel32: move i386 specific ExitProcess() into separate source.
    this improves LTO coverage and ensures that this symbol is accounted.
    
    Signed-off-by: Konstantin Demin <rockdrilla@gmail.com>

--- dlls/kernel32/process.c
+++ dlls/kernel32/process.c
@@ -179,15 +179,9 @@ DWORD WINAPI LoadModule( LPCSTR name, LPVOID paramBlock )
  *  Nothing.
  */
 #ifdef __i386__
-__ASM_STDCALL_FUNC( ExitProcess, 4, /* Shrinker depend on this particular ExitProcess implementation */
-                   "pushl %ebp\n\t"
-                   ".byte 0x8B, 0xEC\n\t" /* movl %esp, %ebp */
-                   ".byte 0x6A, 0x00\n\t" /* pushl $0 */
-                   ".byte 0x68, 0x00, 0x00, 0x00, 0x00\n\t" /* pushl $0 - 4 bytes immediate */
-                   "pushl 8(%ebp)\n\t"
-                   "call " __ASM_STDCALL("RtlExitUserProcess",4) "\n\t"
-                   "leave\n\t"
-                   "ret $4" )
+
+/* defined in "asm_i386_exitprocess.c" */
+
 #else
 
 void WINAPI ExitProcess( DWORD status )
