64a44d470d14924d19990f406c6690ad7c8de416
Rémi Bernon <rbernon@codeweavers.com>
2026-02-13 15:30:53 +0100

    ntdll: Send process PEB / initial TEB in get_startup_info.

--- server/process.c
+++ server/process.c
@@ -1494,6 +1494,9 @@ DECL_HANDLER(get_startup_info)
     struct startup_info *info = process->startup_info;
     data_size_t size;
 
+    current->teb = req->teb;
+    process->peb = req->peb;
+
     if (!info) return;
 
     /* we return the data directly without making a copy so this can only be called once */
@@ -1516,9 +1519,6 @@ DECL_HANDLER(init_process_done)
         set_error( STATUS_INVALID_PARAMETER );
         return;
     }
-
-    current->teb = req->teb;
-    process->peb = req->peb;
 
     process->start_time = current_time;
 
