searchingin/wine-9.0

msxml3: IXMLDOMDocument3::preserveWhiteSpace fix for a non VARIANT_BOOL value.
0024:trace:msxml:domdoc_put_preserveWhiteSpace (068F0520)->(1)

Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=21940

dlls/msxml3/domdoc.c
@@ -2746,7 +2746,7 @@ static HRESULT WINAPI domdoc_put_preserveWhiteSpace(
 {
     domdoc *This = impl_from_IXMLDOMDocument3( iface );
     TRACE("(%p)->(%d)\n", This, isPreserving);
-    This->properties->preserving = isPreserving;
+    This->properties->preserving = isPreserving == VARIANT_TRUE ? VARIANT_TRUE : VARIANT_FALSE;
     return S_OK;
 }
 

RealSupremium/wine

dlls/msxml3/domdoc.c
@@ -1673,7 +1673,7 @@ static HRESULT WINAPI domdoc_put_preserveWhiteSpace(IXMLDOMDocument3 *iface, VAR
 
     TRACE("%p, %d.\n", iface, v);
 
-    node->properties->preserving = v;
+    node->properties->preserving = v == VARIANT_TRUE ? VARIANT_TRUE : VARIANT_FALSE;
     return S_OK;
 }
 