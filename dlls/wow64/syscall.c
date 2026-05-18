cf46331cb114251d2243d7c823be62e6b9388de0
Zhiyi Zhang <zzhang@codeweavers.com>
2026-05-18 10:08:16 +0800

    ntdll: Add NtAlpcDisconnectPort() stub.

--- dlls/wow64/syscall.c
+++ dlls/wow64/syscall.c
@@ -621,6 +621,17 @@ NTSTATUS WINAPI wow64_NtAlpcCreatePort( UINT *args )
 }
 
 /**********************************************************************
+ *           wow64_NtAlpcDisconnectPort
+ */
+NTSTATUS WINAPI wow64_NtAlpcDisconnectPort( UINT *args )
+{
+    HANDLE handle = get_handle( &args );
+    ULONG flags = get_ulong( &args );
+
+    return NtAlpcDisconnectPort( handle, flags );
+}
+
+/**********************************************************************
  *           wow64_NtAlpcSendWaitReceivePort
  */
 NTSTATUS WINAPI wow64_NtAlpcSendWaitReceivePort( UINT *args )
